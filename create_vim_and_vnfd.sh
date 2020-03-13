7527ba54aec0b44c22e4cdfaaa417ebeaa9eeee4
hotephen <ghcks1000@gmail.com>
2020-03-13 18:30:09 +0900

    commit

---
+++ create_vim_and_vnfd.sh
@@ -0,0 +1,5 @@
+tacker vim-register --id-default --config-file ~/etc/toscak/vim_config.yaml vim
+
+tacker vnfd-create --vnfd-file ~/etc/jw/VNFFG-VNFD1.yaml VNFD1
+
+tacker vnfd-create --vnfd-file ~/etc/jw/VNFFG-VNFD2.yaml VNFD2
